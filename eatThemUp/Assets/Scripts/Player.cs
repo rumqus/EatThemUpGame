66d4fcfa8a2bb845b92cdaaf30c3eadb510bffd9
rumqus <commonthings@yandex.ru>
2022-12-06 22:31:10 +0300

    PlayerSuperSize

--- eatThemUp/Assets/Scripts/Player.cs
+++ eatThemUp/Assets/Scripts/Player.cs
@@ -10,7 +10,8 @@ public class Player : MonoBehaviour
     private Vector3 stepPosition = new Vector3(0, 1f, 0f); // step of changing player position against Y axis
     [SerializeField] private float speedGrowth = 2; // speed of growth Player
     [SerializeField] private float stepSize = 0.3f; // step of increasing size player
-    [SerializeField] private float timeSuperSize; // taimer for super-size
+    [SerializeField] private float timer;
+
     // debug panel
     public float enemySizeS;
     public float enemyLevelS;
@@ -24,12 +25,13 @@ public class Player : MonoBehaviour
     {
         pointsSize = 1f;
         levelOfSize = 1;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     /// <summary>
@@ -38,36 +40,60 @@ public class Player : MonoBehaviour
     private void ChangeSize(float enemySize, float enemyLevel)
     {
         pointsSize += enemySize; //прибавляем очки роста
-        if (levelOfSize <= maxLevelofSize && pointsSize / 5 >= 1 && levelOfSize > enemyLevel)
+
+        //if (levelOfSize <= maxLevelofSize && pointsSize / 5 >= 1 && levelOfSize > enemyLevel)
+        //{
+        //    levelOfSize++;
+        //    transform.localScale += new Vector3(stepSize, stepSize, stepSize);
+        //    transform.position = Vector3.Lerp(transform.position, transform.position + stepPosition, speedGrowth * Time.deltaTime);
+        //    pointsSize = 1f;
+        //}
+        //else if (levelOfSize == maxLevelofSize && pointsSize / 5 >= 1 && levelOfSize > enemyLevel)
+        //{
+        //    GetSuperSize();
+        //}
+
+        if (LevelOfsize > enemyLevel)
         {
-            levelOfSize++;
-            transform.localScale += new Vector3(stepSize, stepSize, stepSize);
-            transform.position = Vector3.Lerp(transform.position, transform.position + stepPosition, speedGrowth * Time.deltaTime);
-            pointsSize = 1f;
+            if (pointsSize / 5 > 1)
+            {
+                if (levelOfSize < maxLevelofSize)
+                {
+                    levelOfSize++;
+                    transform.localScale += new Vector3(stepSize, stepSize, stepSize);
+                    transform.position = Vector3.Lerp(transform.position, transform.position + stepPosition, speedGrowth * Time.deltaTime);
+                    pointsSize = 1f;
+                }
+                else
+                {
+                    GetSuperSize();
+                }
+
+            }
+
         }
-        else
-        {
-            // даем увеличение на время
-        }
+
         enemyLevelS = enemyLevel;
         enemySizeS = enemySize;
         debugPanel.GetComponent<DebugPanel>().Showdata();
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
+        // detecting collision with enemy
         if (other.TryGetComponent<Enemy>(out Enemy enemy))
         {
-            
+            // compare size with player vs enemy
             if (CompareSize(enemy.Size))
             {
+                //if true - player is bigger
                 ChangeSize(enemy.Size, enemy.LevelofSize);
                 Destroy(other.gameObject);
             }
             else
             {
+                // end of the games
                 Debug.Log("Конец игры");
                 //жрем игрока
                 //отыгрываем анимацию пожирания
@@ -76,9 +102,6 @@ public class Player : MonoBehaviour
             }
 
         }
-
-
-
     }
 
     /// <summary>
@@ -95,6 +118,23 @@ public class Player : MonoBehaviour
         else return false;
 
     }
+    /// <summary>
+    /// player get super size to eat biggest enemy, переделать что бы игрок уменьшался в изначальное состояние
+    /// </summary>
+    private void GetSuperSize()
+    {
+        transform.localScale = new Vector3(3f, 3f, 3f);
+        StartCoroutine(TimeSuperSize(timer));
+    }
+
+    private IEnumerator TimeSuperSize(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        pointsSize = 1f;
+        levelOfSize = 1;
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
 
 
 }
