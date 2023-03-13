c9d6c2ca99abbf034e03b93502b6e749ab361f8d
rumqus <commonthings@yandex.ru>
2023-03-13 22:28:01 +0300

    moving platform proto

---
+++ eatThemUp/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject movingPlatform;
+    private int currentWaypoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (waypoints.Count <= 0) return;
+        currentWaypoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    void MovePlatform() 
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position,
+            (moveSpeed * Time.deltaTime));
+
+        if (Vector3.Distance(waypoints[currentWaypoint].transform.position, transform.position) <= 0)
+        {
+            currentWaypoint++;
+        }
+
+        if (currentWaypoint != waypoints.Count) return;
+        waypoints.Reverse();
+        currentWaypoint = 0;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        player.transform.parent = movingPlatform.transform;
+            //transform.parent = transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        player.transform.parent = null;
+    }
+}
