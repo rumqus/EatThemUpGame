12847cb484f63d2746629dec8287c5dca5ceb8bc
rumqus <commonthings@yandex.ru>
2023-03-15 22:48:14 +0300

    problem with platform
    zero moving platform

--- eatThemUp/Assets/Scripts/MovingPlatform.cs
+++ eatThemUp/Assets/Scripts/MovingPlatform.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    [SerializeField] private List<Transform> waypoints;
+    private List<Transform> waypoints;
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject movingPlatform;
+    private Transform wayPointUp;
+    private Transform wayPointDown;
     private int currentWaypoint;
+
+    private void Awake()
+    {
+        wayPointUp.position = new Vector3(movingPlatform.transform.position.x, movingPlatform.transform.position.y + 5, movingPlatform.transform.position.z);
+        wayPointDown.position = new Vector3(movingPlatform.transform.position.x, movingPlatform.transform.position.y - 5, movingPlatform.transform.position.z);
+        waypoints.Add(wayPointUp);
+        waypoints.Add(wayPointDown);
+    }
     // Start is called before the first frame update
     void Start()
     {
+        
         if (waypoints.Count <= 0) return;
         currentWaypoint = 0;
     }
