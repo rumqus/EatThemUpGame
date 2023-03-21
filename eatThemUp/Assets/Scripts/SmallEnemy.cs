c28d8be3b55e8f90367bdd017b94232f794da0de
rumqus <commonthings@yandex.ru>
2023-03-21 22:25:47 +0300

    problem with spawniong enemy
    spwan and move bottom

--- eatThemUp/Assets/Scripts/SmallEnemy.cs
+++ eatThemUp/Assets/Scripts/SmallEnemy.cs
@@ -13,7 +13,7 @@ public class SmallEnemy : Enemy
         target = PlayerInstance.instancePlayer.player.transform;
         Agent.avoidancePriority = Random.Range(76, 99);
         Up = true;
-        upPosition = new Vector3(ChildGO.transform.position.x, 0.8f, ChildGO.transform.position.z);
+        upPosition = new Vector3(ChildGO.transform.position.x, 11f, ChildGO.transform.position.z);
     }
 
     private void Update()
