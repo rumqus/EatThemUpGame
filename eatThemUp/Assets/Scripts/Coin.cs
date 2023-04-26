4cda8b916d98029a595b2c5b7ffa6d0e260d13ca
rumqus <commonthings@yandex.ru>
2023-04-26 22:11:40 +0300

    coinsLabel and coinPrefab

--- eatThemUp/Assets/Scripts/Coin.cs
+++ eatThemUp/Assets/Scripts/Coin.cs
@@ -9,13 +9,9 @@ public class Coin : Enemy
     // Start is called before the first frame update
     void Start()
     {
-        Size = 0.4f;
-        LevelofSize = 0.4f;
         Agent = gameObject.GetComponent<NavMeshAgent>();
         target = PlayerInstance.instancePlayer.player.transform;
-        Agent.avoidancePriority = Random.RandomRange(50, 75);
-        Up = true;
-        upPosition = new Vector3(ChildGO.transform.position.x, 0.8f, ChildGO.transform.position.z);
+        Agent.avoidancePriority = Random.RandomRange(50, 75);        
     }
 
     private void Update()
