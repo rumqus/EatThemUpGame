cb590fc5718aaee0fc15a1b33e7bed91e93c0d83
rumqus <commonthings@yandex.ru>
2023-06-18 19:53:12 +0300

    ui in process
    done
    navigation between scenes

--- eatThemUp/Assets/Scripts/UIGame.cs
+++ eatThemUp/Assets/Scripts/UIGame.cs
@@ -3,33 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIButtons : MonoBehaviour
+public class UIGame : MonoBehaviour
 {
     //[SerializeField] private GameObject startButton;
-    //[SerializeField] private GameObject UIpanel;
+    [SerializeField] private GameObject UIpanel;
+    [SerializeField] private float transTime;
     //[SerializeField] private GameObject soundButton;
     //[SerializeField] private GameObject pauseButton;
     //[SerializeField] private GameObject unPauseButton;
-      [SerializeField] private Animator transAnimator;
-      [SerializeField] private float transTime;
+    
+    [SerializeField] private Animator transAnimator;
 
-    private void Start()
+    private void Awake()
     {
         transAnimator.speed = 0;
     }
 
-    public void LoadScene(int index) 
+
+    private void Start()
     {
+        UIpanel.SetActive(false);
+    }
+
+    public void LoadScene(int index)
+    {
+
         transAnimator.speed = 1;
         Time.timeScale = 1;
         StartCoroutine(LoadDelay(index));
     }
 
-    IEnumerator LoadDelay(int index) 
+    IEnumerator LoadDelay(int index)
     {
         transAnimator.Play("FadeEnd");
         yield return new WaitForSeconds(transTime);
         SceneManager.LoadScene(index);
+    }
+
+    public void Pause() 
+    {
+        Debug.Log("click");
+        UIpanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnPause() 
+    {
+        UIpanel.SetActive(false);
+        Time.timeScale = 1;
     }
 
 }
